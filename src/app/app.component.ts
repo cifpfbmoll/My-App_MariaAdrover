d61d0c907513d833d7a7dc92834dcb766a9ad409
Maria Adrover <maria.adrover.v@gmail.com>
2019-01-31 18:00:05 +0100

    14ex. Data Binding (part 1 + part 2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'My 1rrrrrst my-app';
+
+  /*1st Data binding exercise -->app.component.html*/
+  /*
+  onClickMe(textoEnviado) {
+    alert(textoEnviado);
+  }
+
+  clickMessage = '';
+  clicks = 1;
+  onClickMe2(event: any) {
+    let evMsg = event ? ' Event target is ' + event.target.tagName : '';
+    this.clickMessage = (`Click ${this.clicks++}. ${evMsg}`);
+  }
+  */  
 }
